085b4bde2f460cced39926796eedbbfa3fc7a457
John Netzel <johnrnetzel@gmail.com>
2022-09-28 21:13:07 -0800

    ConverterApplication now converts and prints results of RNG picked values

--- M1-Challenge-Netzel-John/ConverterInterface/src/main/java/com/company/ConverterApplication.java
+++ M1-Challenge-Netzel-John/ConverterInterface/src/main/java/com/company/ConverterApplication.java
@@ -20,8 +20,20 @@ public class ConverterApplication {
         int testDayOfWeek2 = RNG.nextInt(NUM_DAYS_IN_WEEK) + 1;
         int testDayOfWeek3 = RNG.nextInt(NUM_DAYS_IN_WEEK) + 1;
 
+        // Print test input
         System.out.println("Test month values are: " + testMonth1 + ", " + testMonth2 + ", " + testMonth3);
         System.out.println("Test day of week values are: " + testDayOfWeek1 + ", " + testDayOfWeek2 + ", " +
                 testDayOfWeek3);
+
+        // Print output
+        System.out.println("If converted month values are: " + converterIf.convertMonth(testMonth1) + ", " +
+                converterIf.convertMonth(testMonth2) + ", " + converterIf.convertMonth(testMonth3));
+        System.out.println("Switch converted month values are: " + converterSwitch.convertMonth(testMonth1) + ", " +
+                converterSwitch.convertMonth(testMonth2) + ", " + converterSwitch.convertMonth(testMonth3));
+
+        System.out.println("If converted day of week values are: " + converterIf.convertDay(testDayOfWeek1) + ", " +
+                converterIf.convertDay(testDayOfWeek2) + ", " + converterIf.convertDay(testDayOfWeek3));
+        System.out.println("Switch converted day of week values are: " + converterSwitch.convertDay(testDayOfWeek1) + ", " +
+                converterSwitch.convertDay(testDayOfWeek2) + ", " + converterSwitch.convertDay(testDayOfWeek3));
     }
 }
