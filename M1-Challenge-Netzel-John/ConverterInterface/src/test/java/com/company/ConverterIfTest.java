0c19533da0477eed6492c6c387ac607828dc2eb9
John Netzel <johnrnetzel@gmail.com>
2022-09-29 19:00:34 -0800

    Add unit tests for basic ConverterIf usage

--- M1-Challenge-Netzel-John/ConverterInterface/src/test/java/com/company/ConverterIfTest.java
+++ M1-Challenge-Netzel-John/ConverterInterface/src/test/java/com/company/ConverterIfTest.java
@@ -6,16 +6,46 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConverterIfTest {
+    private Converter converter;
 
     @Before
     public void setUp() {
+        converter = new ConverterIf();
     }
 
     @Test
-    public void convertMonth() {
+    public void shouldConvertNumberToMonth() {
+        // Arrange
+        String expectedValue1 = "March";
+        String expectedValue2 = "April";
+        String expectedValue3 = "December";
+
+        // Act
+        String actualValue1 = converter.convertMonth(3);
+        String actualValue2 = converter.convertMonth(4);
+        String actualValue3 = converter.convertMonth(12);
+
+        // Assert
+        assertEquals(expectedValue1, actualValue1);
+        assertEquals(expectedValue2, actualValue2);
+        assertEquals(expectedValue3, actualValue3);
     }
 
     @Test
-    public void convertDay() {
+    public void shouldConvertNumberToDay() {
+        // Arrange
+        String expectedValue1 = "Sunday";
+        String expectedValue2 = "Wednesday";
+        String expectedValue3 = "Saturday";
+
+        // Act
+        String actualValue1 = converter.convertDay(1);
+        String actualValue2 = converter.convertDay(4);
+        String actualValue3 = converter.convertDay(7);
+
+        // Assert
+        assertEquals(expectedValue1, actualValue1);
+        assertEquals(expectedValue2, actualValue2);
+        assertEquals(expectedValue3, actualValue3);
     }
 }
