2cea3778f8435eb0dc1ed3f831276a7ae173a7d4
John Netzel <johnrnetzel@gmail.com>
2022-09-29 20:12:15 -0800

    Create unit tests for floating-point arithmetic

--- M1-Challenge-Netzel-John/CalculatorObject/src/test/java/com/company/CalculatorTest.java
+++ M1-Challenge-Netzel-John/CalculatorObject/src/test/java/com/company/CalculatorTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+    // The amount of error tolerance for floating-point arithmetic
+    private static final double FP_DELTA = 0.001;
     private Calculator calculator;
 
     @Before
@@ -79,17 +81,65 @@ public class CalculatorTest {
 
     @Test
     public void shouldAddDoubles() {
+        // Arrange
+        double expectedResult1 = 2.0;
+        double expectedResult2 = -333.38;
+        double expectedResult3 = 50.1;
+        // Act
+        double actualResult1 = calculator.add(1.0, 1.0);
+        double actualResult2 = calculator.add(123.4, -456.78);
+        double actualResult3 = calculator.add(50.1, 0.0);
+        // Assert
+        assertEquals(expectedResult1, actualResult1, FP_DELTA);
+        assertEquals(expectedResult2, actualResult2, FP_DELTA);
+        assertEquals(expectedResult3, actualResult3, FP_DELTA);
     }
 
     @Test
     public void shouldSubtractDoubles() {
+        // Arrange
+        double expectedResult1 = 0.0;
+        double expectedResult2 = 580.18;
+        double expectedResult3 = 50.1;
+        // Act
+        double actualResult1 = calculator.subtract(1.0, 1.0);
+        double actualResult2 = calculator.subtract(123.4, -456.78);
+        double actualResult3 = calculator.subtract(50.1, 0.0);
+        // Assert
+        assertEquals(expectedResult1, actualResult1, FP_DELTA);
+        assertEquals(expectedResult2, actualResult2, FP_DELTA);
+        assertEquals(expectedResult3, actualResult3, FP_DELTA);
     }
 
     @Test
     public void shouldMultiplyDoubles() {
+        // Arrange
+        double expectedResult1 = 1.0;
+        double expectedResult2 = -56366.652;
+        double expectedResult3 = 0.0;
+        // Act
+        double actualResult1 = calculator.multiply(1.0, 1.0);
+        double actualResult2 = calculator.multiply(123.4, -456.78);
+        double actualResult3 = calculator.multiply(50.1, 0.0);
+        // Assert
+        assertEquals(expectedResult1, actualResult1, FP_DELTA);
+        assertEquals(expectedResult2, actualResult2, FP_DELTA);
+        assertEquals(expectedResult3, actualResult3, FP_DELTA);
     }
 
     @Test
     public void shouldDivideDoubles() {
+        // Arrange
+        double expectedResult1 = 1.0;
+        double expectedResult2 = -0.270151;
+        double expectedResult3 = 0.0;
+        // Act
+        double actualResult1 = calculator.divide(1.0, 1.0);
+        double actualResult2 = calculator.divide(123.4, -456.78);
+        double actualResult3 = calculator.divide(0.0, 50.1);
+        // Assert
+        assertEquals(expectedResult1, actualResult1, FP_DELTA);
+        assertEquals(expectedResult2, actualResult2, FP_DELTA);
+        assertEquals(expectedResult3, actualResult3, FP_DELTA);
     }
 }
