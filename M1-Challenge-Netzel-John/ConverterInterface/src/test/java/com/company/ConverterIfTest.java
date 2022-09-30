c88bc0d33ae21dd3b53a7f593c26b8bfa48a2ecd
John Netzel <johnrnetzel@gmail.com>
2022-09-29 18:47:07 -0800

    Add JUnit dependency to Maven

---
+++ M1-Challenge-Netzel-John/ConverterInterface/src/test/java/com/company/ConverterIfTest.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterIfTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void convertMonth() {
+    }
+
+    @Test
+    public void convertDay() {
+    }
+}
