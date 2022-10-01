a17ccdc0ff2c8a80f81fe4daf7e1c8d21565c84f
John Netzel <johnrnetzel@gmail.com>
2022-09-30 17:19:33 -0800

    Create nontrivial methods for vendor IceCream class

--- M1-Challenge-Netzel-John/IceCreamClassDesign/src/main/java/com/company/vendor/IceCream.java
+++ M1-Challenge-Netzel-John/IceCreamClassDesign/src/main/java/com/company/vendor/IceCream.java
@@ -1,5 +1,7 @@
 package com.company.vendor;
 
+import java.util.Date;
+
 public class IceCream {
     private static final double MELTING_POINT = 32.0; // in Fahrenheit
 
@@ -8,13 +10,14 @@ public class IceCream {
     private String flavor;
     private double temperature;
     private boolean sold;
+    private Date openedTime; // The time the batch that this ice cream is from was opened
 
-    public IceCream()
+    public IceCream(double servingSize, double cost, String flavor)
     {
-        this.servingSize = 2.0; // 2 fl oz, a small scoop
-        this.cost = 0.0; // free ice cream!
-        this.flavor = null;
-        this.temperature = 0; // Intended to be 0 degrees Fahrenheit, well below the melting point of water
+        this.servingSize = servingSize;
+        this.cost = cost;
+        this.flavor = flavor;
+        this.temperature = 0.0; // Intended to be 0 degrees Fahrenheit, well below the melting point of water
         this.sold = false;
     }
 
@@ -22,48 +25,12 @@ public class IceCream {
         return servingSize;
     }
 
-    public void setServingSize(double servingSize) {
-        if(servingSize > 0.0) {
-            this.servingSize = servingSize;
-        }
-        else {
-            System.out.println("Can't set negative or 0 serving size.");
-        }
-    }
-
     public double getCost() {
         return cost;
     }
 
-    public void setCost(double cost) {
-        if(cost >= 0.0) {
-            this.cost = cost;
-        }
-        else {
-            System.out.println("We're not paying people to buy our ice cream!");
-        }
-    }
-
     public String getFlavor() {
-        if(flavor == null) {
-            return "unknown";
-        }
-        if(flavor.equals("")) {
-            return "unknown";
-        }
         return flavor;
-    }
-
-    public void setFlavor(String flavor) {
-        if(flavor == null) {
-            System.out.println("Ice cream must have a flavor.");
-        }
-        else if(flavor.equals("")) {
-            System.out.println("Ice cream must have a flavor.");
-        }
-        else {
-            this.flavor = flavor;
-        }
     }
 
     public double getTemperature() {
@@ -78,14 +45,32 @@ public class IceCream {
         return sold;
     }
 
-    public String sell() {
-        if(getFlavor().equals("unknown")) {
-            return "Can't sell ice cream without knowing its flavor!";
-        }
-        return null;
+    public Date getOpenedTime() {
+        return openedTime;
+    }
+
+    public void open() {
+        openedTime = new Date(); // Date constructor with no params defaults to current time
+    }
+
+    public void sell() {
+        sold = true;
+        // Probably want code to credit vendor for the cost
+    }
+
+    public boolean isOpened() {
+        return getOpenedTime() == null; // null means "hasn't been opened yet"
     }
 
     public boolean isMelted() {
-        return false;
+        return getTemperature() >= 32.0; // 32 degrees Fahrenheit isn't fully melted, but who wants to eat half-melted mush?
+    }
+
+    // Is the ice cream spoiled and eligible to be written off as a cost of business?
+    public boolean isSpoiledForWriteOff() {
+        if(isSold()) {
+            return false; // Ice cream that has already been sold is the customer's problem
+        }
+        return isMelted() && !isOpened(); // Ice cream must be melted and unopened for write off
     }
 }
