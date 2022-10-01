b8baa6bb760aee5ef6efd1cc6a20fd7679086bf5
John Netzel <johnrnetzel@gmail.com>
2022-09-30 18:39:26 -0800

    Create derived classes of Character

---
+++ M1-Challenge-Netzel-John/RPGInheritance/src/main/java/com/company/Constable.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Constable extends Character {
+    private String jurisdiction;
+
+    public Constable(String name) {
+        super(name, 60, 100, 60, 20, 5);
+    }
+
+    public void arrest(Character target) {
+
+    }
+}
