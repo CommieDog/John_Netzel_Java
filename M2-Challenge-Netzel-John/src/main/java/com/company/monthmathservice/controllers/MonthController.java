07616bc0db6c639d124dafe3843bcd6b387653bd
John Netzel <johnrnetzel@gmail.com>
2022-10-04 21:13:21 -0800

    Implement behavior for month controller

--- M2-Challenge-Netzel-John/src/main/java/com/company/monthmathservice/controllers/MonthController.java
+++ M2-Challenge-Netzel-John/src/main/java/com/company/monthmathservice/controllers/MonthController.java
@@ -5,15 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class MonthController {
+    // Each instance of controller has own RNG
+    private final Random RNG = new Random();
+
     @GetMapping(value = "/month/{monthNumber}")
     public Month getMonthByNumber(@PathVariable int monthNumber) {
-        return new Month(1);
+        return new Month(monthNumber);
     }
 
     @GetMapping(value = "/randomMonth")
     public Month getRandomMonth() {
-        return new Month(2);
+        int monthNumber = RNG.nextInt(12) + 1; // rng number range is 0..11
+        return new Month(monthNumber);
     }
 }
