ec6d707593554c0226a1b7f9c23c29b633c9cccf
John Netzel <johnrnetzel@gmail.com>
2022-10-04 20:28:56 -0800

    Add Spring Boot project for Module 2 Challenge

---
+++ M2-Challenge-Netzel-John/src/main/java/com/company/monthmathservice/MonthMathServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.monthmathservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthMathServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthMathServiceApplication.class, args);
+	}
+
+}
---
+++ M2-Challenge-Netzel-John/src/test/java/com/company/monthmathservice/MonthMathServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.monthmathservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthMathServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
