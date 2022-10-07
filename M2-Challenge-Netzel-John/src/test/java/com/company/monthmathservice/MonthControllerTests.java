546bb9b5cf592f204f99896017d8afed4574a9c7
John Netzel <johnrnetzel@gmail.com>
2022-10-06 21:36:56 -0800

    Write tests for Month controller

--- M2-Challenge-Netzel-John/src/test/java/com/company/monthmathservice/MonthControllerTests.java
+++ M2-Challenge-Netzel-John/src/test/java/com/company/monthmathservice/MonthControllerTests.java
@@ -1,6 +1,8 @@
 package com.company.monthmathservice;
 
 import com.company.monthmathservice.controllers.MonthController;
+import com.company.monthmathservice.models.Month;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +18,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MonthControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@Test
 	public void shouldGetMonthByNumberReturnMonthName() throws Exception {
-		String expectedVal = "April";
+		String expectedVal = mapper.writeValueAsString(new Month(4));
 
 		mockMvc.perform(get("/month/{monthNumber}", 4))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.name").value(expectedVal));
+				.andExpect(content().json(expectedVal));
+	}
+
+	@Test
+	public void shouldFailGetMonthByNumberWithNumberOutOfBounds() throws Exception {
+		mockMvc.perform(get("/month/{monthNumber}", 0))
+				.andExpect(status().isUnprocessableEntity());
+		mockMvc.perform(get("/month/{monthNumber}", 13))
+				.andExpect(status().isUnprocessableEntity());
+	}
+
+	@Test
+	public void shouldGetRandomMonth() throws Exception {
+		mockMvc.perform(get("/randomMonth"))
+				.andExpect(status().isOk());
 	}
 }
