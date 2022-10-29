fc146183a851313f8b219517d73f54a371c7039c
John Netzel <johnrnetzel@gmail.com>
2022-10-29 15:41:26 -0800

    Add service registry project

---
+++ M5-Challenge-Netzel-John/gamestore-service-registry/src/main/java/com/company/gamestoreserviceregistry/GamestoreServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.company.gamestoreserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestoreServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestoreServiceRegistryApplication.class, args);
+	}
+
+}
